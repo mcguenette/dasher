2c39dde3fa178320ec3881b3c1808bea3e500d67
MC Guenette <mc.c.guenette@gmail.com>
2023-12-13 22:35:17 -0600

    fixed map and adjusted styling

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,44 +1,40 @@
 'use strict';
+import { select } from './utils.js';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWNndWVuZXR0ZSIsImEiOiJjbHExOWUxeWcwNmwyMmlvMGY3NXF3bGc4In0.SXaq4QutArp0bqPMpmnkjg';
 
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mcguenette/clq46ofev004c01pdacot3c5t', // style URL
-    center: [-97.1418535214156, 49.89382907977702],  // starting position [lng, lat]
-    zoom: 9, // starting zoom,
+    container: 'map',
+    style: 'mapbox://styles/mcguenette/clq46ofev004c01pdacot3c5t',
+    center: [-97.1418535214156, 49.89382907977702],
+    zoom: 9,
     pitch: 40
 });
 
+const trackButton = select('#tracking');
 let userLocation;
 
-map.on('load', () => {
-    map.addLayer({
-        id: 'terrain-data',
-        type: 'line',
-        source: {
-            type: 'vector',
-            url: 'mapbox://mapbox.mapbox-terrain-v2'
-        },
-        'source-layer': 'contour'
-    });
-});
-
 function addMarker(coordinates) {
-    const el = document.createElement('div');
-    el.className = 'marker';
-
-    // Make a marker for the feature and add it to the map
-    new mapboxgl.Marker(el)
-        .setLngLat(coordinates)
+    // Grabbed from: https://docs.mapbox.com/mapbox-gl-js/api/markers/#marker-example
+    let marker = new mapboxgl.Marker({
+        color: "#860D0D",
+        width: '10px',
+        height: '10px',
+        draggable: true
+        }).setLngLat(coordinates)
         .addTo(map);
+    // const el = document.createElement('div');
+    // el.className = 'marker';
+
+    // new mapboxgl.Marker(el)
+    //     .setLngLat(coordinates)
+    //     .addTo(map);
 }
 
 function getLocation(position) {
     let { latitude, longitude } = position.coords;
     userLocation = [longitude, latitude];
     map.flyTo({ center: userLocation, zoom: 14 });
-
     addMarker(userLocation);
 }
 
@@ -50,37 +46,13 @@ const options = {
     enableHighAccuracy: true
 };
 
-if ('geolocation' in navigator) {
-    navigator.geolocation.getCurrentPosition(getLocation, errorHandler, options);
-} else {
-    alert('Browser does not support geolocation');
-}
-
-const geojson = {
-    type: 'FeatureCollection',
-    features: [
-        {
-            type: 'Feature',
-            geometry: {
-                type: 'Point',
-                coordinates: userLocation ? [userLocation[0], userLocation[1]] : [0, 0]
-            },
-            properties: {
-                title: 'My Location'
-            }
-        },
-    ]
-};
-
-// Add markers to map
-for (const feature of geojson.features) {
-    // create an HTML element for each feature
-    const el = document.createElement('div');
-    el.className = 'marker';
-
-    // make a marker for each feature and add it to the map
-    new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map);
-}
+setTimeout(() => {
+    if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(getLocation, errorHandler, options);
+    } else {
+        alert('Browser does not support geolocation');
+    }
+}, 1000);
 
 map.addControl(
     new mapboxgl.GeolocateControl({
@@ -91,3 +63,12 @@ map.addControl(
         showUserHeading: true
     })
 );
+
+// Event listener for the tracking button
+trackButton.addEventListener('click', () => {
+    if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(getLocation, errorHandler, options);
+    } else {
+        alert('Browser does not support geolocation');
+    }
+});
